72c5b16b2cbd44edc02b7dc05b94467708572dc7
Nick <nlogozzo225@gmail.com>
2022-05-08 00:41:27 -0400

    V2022.5.2
    - Added UriExtensions

---
+++ Nickvision.WPF/Extensions/UriExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Nickvision.WPF.Extensions;
+
+/// <summary>
+/// Extension methods for Uri.
+/// </summary>
+public static class UriExtensions
+{
+    /// <summary>
+    /// Opens a Uri in the default browser.
+    /// </summary>
+    /// <param name="uri">The Uri to open</param>
+    public static void OpenInBrowser(this Uri uri)
+    {
+        var url = uri.ToString();
+        url.Replace("&", "^&");
+        Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+    }
+}
