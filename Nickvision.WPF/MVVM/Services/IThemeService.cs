452bd9192dc9b8f7f6c76dafa35863d3d9820ab0
Nick <nlogozzo225@gmail.com>
2022-05-14 22:46:38 -0400

    V2022.5.6
    - Added ThemeService

---
+++ Nickvision.WPF/MVVM/Services/IThemeService.cs
@@ -0,0 +1,47 @@
+﻿namespace Nickvision.WPF.MVVM.Services;
+
+/// <summary>
+/// The themes an application can have.
+/// </summary>
+public enum Theme
+{
+    Light,
+    Dark,
+    System
+}
+
+/// <summary>
+/// The accent colors an application can have.
+/// </summary>
+public enum AccentColor
+{
+    System,
+    Red,
+    Orange,
+    Yellow,
+    Green,
+    Blue,
+    Purple,
+    Pink,
+    Brown,
+    Black,
+    White
+}
+
+/// <summary>
+/// A service for working with an application's theme.
+/// </summary>
+public interface IThemeService : IService
+{
+    /// <summary>
+    /// Changes the theme of the application.
+    /// </summary>
+    /// <param name="theme">The theme</param>
+    void ChangeTheme(Theme theme);
+
+    /// <summary>
+    /// Changes the accent color of the application.
+    /// </summary>
+    /// <param name="theme">The accent color</param>
+    void ChangeAccentColor(AccentColor accentColor);
+}
---
+++ Nickvision.WPF/MVVM/Services/ThemeService.cs
@@ -0,0 +1,56 @@
+﻿using HandyControl.Themes;
+using MicaWPF;
+using System.Windows.Media;
+
+namespace Nickvision.WPF.MVVM.Services;
+
+/// <summary>
+/// A service for working with an application's theme.
+/// </summary>
+public class ThemeService : IThemeService
+{
+    /// <summary>
+    /// Changes the theme of the application.
+    /// </summary>
+    /// <param name="theme">The theme</param>
+    public void ChangeTheme(Theme theme)
+    {
+        MicaWPF.Services.ThemeService.GetCurrent().ChangeTheme(theme switch
+        {
+            Theme.Light => WindowsTheme.Light,
+            Theme.Dark => WindowsTheme.Dark,
+            Theme.System => WindowsTheme.Auto,
+            _ => WindowsTheme.Auto
+        });
+        ThemeManager.Current.ApplicationTheme = theme switch
+        {
+            Theme.Light => ApplicationTheme.Light,
+            Theme.Dark => ApplicationTheme.Dark,
+            Theme.System => null,
+            _ => null
+        };
+    }
+
+    /// <summary>
+    /// Changes the accent color of the application.
+    /// </summary>
+    /// <param name="theme">The accent color</param>
+    public void ChangeAccentColor(AccentColor accentColor)
+    {
+        ThemeManager.Current.AccentColor = accentColor switch
+        {
+            AccentColor.System => null,
+            AccentColor.Red => Brushes.Red,
+            AccentColor.Orange => Brushes.Orange,
+            AccentColor.Yellow => Brushes.Yellow,
+            AccentColor.Green => Brushes.Green,
+            AccentColor.Blue => Brushes.Blue,
+            AccentColor.Purple => Brushes.Purple,
+            AccentColor.Pink => Brushes.HotPink,
+            AccentColor.Brown => Brushes.SaddleBrown,
+            AccentColor.Black => Brushes.Black,
+            AccentColor.White => Brushes.White,
+            _ => null
+        };
+    }
+}
